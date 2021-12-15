060874b4ee21afb0db904e4c424f69179e9dafc5
Rhys Causey <rhys.causey@D2L.com>
2021-12-15 17:40:19 -0500

    Workaround for Safari not emitting error on URL expiry

--- media-player.js
+++ media-player.js
@@ -65,6 +65,14 @@ const DEFAULT_PREVIEW_WIDTH = 160;
 const DEFAULT_PREVIEW_HEIGHT = 90;
 const DEFAULT_LOCALE = 'en';
 
+const SAFARI_EXPIRY_EARLY_SWAP_SECONDS = 10;
+const SAFARI_EXPIRY_MIN_ERROR_EMIT_SECONDS = 30;
+const isSafari = () => navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') === -1;
+const parseUrlExpiry = url => {
+	const urlObj = new URL(url);
+	return urlObj.searchParams ? urlObj.searchParams.get('Expires') : null;
+};
+
 class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMixin(LitElement))) {
 
 	static get properties() {
@@ -550,6 +558,7 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 		this._volume = 1;
 		this._webVTTParser = new window.WebVTTParser();
 		this._playRequested = false;
+		this._timeBeforeSourceSwap = null;
 	}
 
 	get currentTime() {
@@ -836,7 +845,7 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 		super.updated(changedProperties);
 
 		if (changedProperties.has('src') || changedProperties.has('mediaType')) {
-			this._reloadSource({ forceReload: changedProperties.get('src') !== undefined });
+			this._reloadSource();
 		}
 
 		if (changedProperties.has('locale')) {
@@ -1073,7 +1082,7 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 						@timeupdate=${this._onTimeUpdate}
 						@volumechange=${this._onVolumeChange}
 					>
-						<source src="${this._getCurrentSource()}" @error=${this._onError}>
+						<source @error=${this._onError}>
 					</video>
 				`;
 			case SOURCE_TYPES.audio:
@@ -1096,7 +1105,7 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 						@timeupdate=${this._onTimeUpdate}
 						@volumechange=${this._onVolumeChange}
 					>
-						<source src="${this.src || this._sources[this._selectedQuality]}" @error=${this._onError}></source>
+						<source @error=${this._onError}></source>
 					</audio>
 
 					<div id="d2l-labs-media-player-audio-bars-container">
@@ -1582,6 +1591,7 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 				observer.observe(node, { attributes: true });
 			});
 			this._updateSources(sourceNodes);
+			this._reloadSource();
 		}
 
 		for (let i = 0; i < nodes.length; i++) {
@@ -1815,14 +1825,29 @@ class MediaPlayer extends FocusVisiblePolyfillMixin(InternalLocalizeMixin(RtlMix
 		this._sources[quality] = node.src;
 	}
 
-	_reloadSource({ forceReload = false } = {}) {
+	_reloadSource() {
 		if (this._media) {
 			const oldSourceNode = this._media.getElementsByTagName('source')[0];
 			const updatedSource = this._getCurrentSource();
-			if (forceReload || oldSourceNode.getAttribute('src') !== updatedSource) {
+			if (oldSourceNode.getAttribute('src') !== updatedSource) {
 				this._loading = true;
 
 				oldSourceNode.setAttribute('src', updatedSource);
+
+				// Note: Safari doesn't emit an error if a URL expires during playback. To work
+				// around this, we manually fire an error slightly before the URL is expected
+				// to expire. Note that this only currently works with URLs that include the
+				// "Expires" query parameter (e.g. CloudFront signed URLs). It's also not a
+				// good long-term solution since it depends on a somewhat accurate client
+				// system time.
+				if (isSafari()) {
+					const expires = parseUrlExpiry(updatedSource);
+					if (expires) {
+						const timeToExpiry = (expires * 1000) - Date.now() - SAFARI_EXPIRY_EARLY_SWAP_SECONDS;
+						const timeoutPeriod = Math.max(timeToExpiry, SAFARI_EXPIRY_MIN_ERROR_EMIT_SECONDS);
+						setTimeout(() => this._onError(), timeoutPeriod);
+					}
+				}
 
 				// Maintain the height while loading the new source to prevent
 				// the video object from resizing temporarily
