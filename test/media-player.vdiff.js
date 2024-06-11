875606f3d4fb59210513732ebf2d14ecb0781a5e
Syeda Azim <Syeda.Azim@D2L.com>
2024-06-11 15:20:02 -0400

    Updating vdiff test

--- test/media-player.vdiff.js
+++ test/media-player.vdiff.js
@@ -2,25 +2,38 @@ import '../media-player.js';
 import { expect, fixture, focusElem, html } from '@brightspace-ui/testing';
 
 describe('d2l-labs-media-player', () => {
-	[
-		'video',
-		'video-with-poster',
-		'audio',
-	].forEach((id) => {
-		it(id, async() => {
-			const elem = await fixture(html`<d2l-labs-media-player></d2l-labs-media-player>`);
-			await expect(elem).to.be.golden();
-		});
+	it('video', async() => {
+		const elem = await fixture(html`<d2l-labs-media-player src="./videos/1_lego.webm" media-type="video"></d2l-labs-media-player>`);
+		await expect(elem).to.be.golden();
 	});
 
-	[
-		'video-with-search',
-		'audio-with-search'
-	].forEach((id) => {
-		it(id, async() => {
-			const elem = await fixture(html`<d2l-labs-media-player></d2l-labs-media-player>`);
-			await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
-			await expect(elem).to.be.golden();
-		});
+	it('video-with-poster', async() => {
+		const elem = await fixture(html`<d2l-labs-media-player src="./videos/1_lego.webm" poster="../demo/sample-poster.png" media-type="video"></d2l-labs-media-player>`);
+		await expect(elem).to.be.golden();
+	});
+
+	it('audio', async() => {
+		const elem = await fixture(html`<d2l-labs-media-player src="./audio/applause.mp3" media-type="audio"></d2l-labs-media-player>`);
+		await expect(elem).to.be.golden();
+	});
+
+	it('video-with-search', async() => {
+		const elem = await fixture(
+			html`
+				<d2l-labs-media-player src="./videos/1_lego.webm" media-type="video">
+					<track src="../demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+				</d2l-labs-media-player>`);
+		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
+		await expect(elem).to.be.golden();
+	});
+
+	it('audio-with-search', async() => {
+		const elem = await fixture(
+			html`
+				<d2l-labs-media-player src="./audio/applause.mp3" media-type="audio">
+					<track src="../demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+				</d2l-labs-media-player>`);
+		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
+		await expect(elem).to.be.golden();
 	});
 });
