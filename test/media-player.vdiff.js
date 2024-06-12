d2aa03b1d2418acc6aac343e8ef4ef7109c89f3f
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-06-12 16:09:25 -0400

    treat paths as absolute

--- test/media-player.vdiff.js
+++ test/media-player.vdiff.js
@@ -6,12 +6,10 @@ describe('d2l-labs-media-player', () => {
 		const elem = await fixture(
 			html`
 			<d2l-labs-media-player 
-				src="./videos/1_lego.webm"
+				src="./test/videos/1_lego.webm"
 				media-type="video">
 			</d2l-labs-media-player>`
 		);
-		elem._loading = false;
-		await elem.updateComplete;
 		await expect(elem).to.be.golden();
 	});
 
@@ -19,13 +17,11 @@ describe('d2l-labs-media-player', () => {
 		const elem = await fixture(
 			html`
 			<d2l-labs-media-player 
-				src="./videos/1_lego.webm" 
-				poster="../demo/sample-poster.png" 
+				src="./test/videos/1_lego.webm" 
+				poster="./demo/sample-poster.png" 
 				media-type="video">
 			</d2l-labs-media-player>`
 		);
-		elem._loading = false;
-		await elem.updateComplete;
 		await expect(elem).to.be.golden();
 	});
 
@@ -33,24 +29,20 @@ describe('d2l-labs-media-player', () => {
 		const elem = await fixture(
 			html`
 			<d2l-labs-media-player
-				src="./audio/applause.mp3"
+				src="./test/audio/applause.mp3"
 				media-type="audio">
 			</d2l-labs-media-player>`
 		);
-		elem._loading = false;
-		await elem.updateComplete;
 		await expect(elem).to.be.golden();
 	});
 
 	it('video-with-search', async() => {
 		const elem = await fixture(
 			html`
-			<d2l-labs-media-player src="./videos/1_lego.webm" media-type="video">
-				<track src="../demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+			<d2l-labs-media-player src="./test/videos/1_lego.webm" media-type="video">
+				<track src="./demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
 			</d2l-labs-media-player>`
 		);
-		elem._loading = false;
-		await elem.updateComplete;
 		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
 		await expect(elem).to.be.golden();
 	});
@@ -58,12 +50,10 @@ describe('d2l-labs-media-player', () => {
 	it('audio-with-search', async() => {
 		const elem = await fixture(
 			html`
-			<d2l-labs-media-player src="./audio/applause.mp3" media-type="audio">
-				<track src="../demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
+			<d2l-labs-media-player src="./test/audio/applause.mp3" media-type="audio">
+				<track src="./demo/sample-vtt-en.vtt" kind="captions" srclang="en" label="English" default>
 			</d2l-labs-media-player>`
 		);
-		elem._loading = false;
-		await elem.updateComplete;
 		await focusElem(elem.shadowRoot.querySelector('#d2l-labs-media-player-search-container'));
 		await expect(elem).to.be.golden();
 	});
