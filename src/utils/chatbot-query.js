3e9726c15adbcd88affbffa72ad79c2a376e1fad
Arthur Chang <Arthur.Chang@D2L.com>
2025-02-13 11:57:52 -0500

    BSPACE-163-add-image-support

--- src/utils/chatbot-query.js
+++ src/utils/chatbot-query.js
@@ -1,14 +1,21 @@
-export async function generateResponse(transcript, question) {
+export async function generateResponse(transcript, base64ImageString, question) {
 	// Replace the URL with the endpoint with the current stages in api gateway
 	const url = '';
 	const data = {
-		prompt: `Here is a video transcript: "${transcript}". Please answer this question base on the video: "${question}"`
+		prompt: `Here is a video transcript: "${transcript}" and here is the screenshot of the video when the question was asked.
+		Please answer this question base on the video: "${question}"
+		Simply answer the question and don't say base on trasncript or screenshot.`,
 	};
+
+	if (base64ImageString !== null && base64ImageString !== '' && base64ImageString !== undefined) {
+		data.image = base64ImageString;
+	}
+
 	try {
 		const response = await fetch(url, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'text/plain',
+				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(data)
 		});
@@ -21,3 +28,4 @@ export async function generateResponse(transcript, question) {
 		console.error('Error:', error);
 	}
 }
+
